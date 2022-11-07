050bf12a55b43d086bc51d1451d7f12b7ebde8fb
NK590 <nathankim0917@gmail.com>
2022-11-07 21:20:17 +0900

    'update'

--- background.js
+++ background.js
@@ -11,10 +11,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
         if (data.count >= 50) {
             let pageNum = parseInt(data.count / 50);
             if (data.count % 50 === 0) {
-                pageNum -= 1
+                pageNum -= 1;
             }
 
-            let page = Math.floor(Math.random() * pageNum);
+            let page = Math.floor(Math.random() * pageNum) + 1;
             url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}+-solved_by:${nickname}&page=${page}`;
             
             fetch(url)
@@ -22,20 +22,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             .then((data2) => {
                 console.log(data2);
                 problemIndex = data2.items[Math.floor(Math.random() * data2.items.length)].problemId;
-                chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-                    chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
-                })
+                // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+                //     chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
+                // })
+                chrome.tabs.create({url:`https://www.acmicpc.net/problem/${problemIndex}`});
             })
         } else if (0 < data.count && data.count < 50) {
             problemIndex = data.items[Math.floor(Math.random() * data.items.length)].problemId;
-            chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-                chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
-            })
+            // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            //     chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
+            // })
+            chrome.tabs.create({url:`https://www.acmicpc.net/problem/${problemIndex}`});
         } else {
-            problemIndex = 0;
             chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-                chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
+                chrome.tabs.sendMessage(tabs[0].id, "no match problems", (response) => {});
             })
         }
+        
     })
 })
--- content.js
+++ content.js
@@ -1,8 +1,5 @@
-chrome.extension.onMessage.addListener((problemIndex, sender, sendResponse) => {
-    if (problemIndex != 0) {
-        let url = `https://www.acmicpc.net/problem/${problemIndex}`;
-        window.open(url);
-    } else {
-        alert('검색 결과가 없습니다.');
+chrome.extension.onMessage.addListener((message, sender, sendResponse) => {
+    if (message === "no match problems") {
+        alert("검색 결과가 없습니다.");
     }
 })
