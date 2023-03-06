0879485c8930d7a88e05787a2bf44746a6a22d23
NK590 <nathankim0917@gmail.com>
2023-03-06 15:22:43 +0900

    update features

--- background.js
+++ background.js
@@ -1,8 +1,24 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    let startTier =  request.tier.charAt(0) + '5';
-    let endTier = request.tier.charAt(0) + '1';
+
+    let startTier = request.tier.charAt(0);
+    let endTier = request.tier.charAt(0);
+    let tierLevel = request.tierLevel;
+    if (tierLevel != undefined && tierLevel != '') {
+        startTier += tierLevel;
+        endTier += tierLevel;
+    } else {
+        startTier += '5';
+        endTier += '1';
+    }
+
+    let language = request.language;
+    let langQuery = '';
+    if (language != undefined && language != '') {
+        langQuery = `+lang:${language}`;
+    }
+
     let nickname = request.nickname;
-    let url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}+-solved_by:${nickname}`;
+    let url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}`;
     let problemIndex;
 
     fetch(url)
@@ -15,7 +31,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             }
 
             let page = Math.floor(Math.random() * pageNum) + 1;
-            url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}+-solved_by:${nickname}&page=${page}`;
+            url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}&page=${page}`;
             
             fetch(url)
             .then((response2) => response2.json())
--- popup/popup.js
+++ popup/popup.js
@@ -3,8 +3,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('randBtn').addEventListener('click', () => {
         let tier = document.getElementById('tierSelect').value;
+        let tierLevel = document.getElementById('tierLevelSelect').value;
         let nickname = document.getElementById('nicknameInput').value;
+        let language = document.getElementById('koreanOnly').checked ? 'ko' : '';
 
-        chrome.runtime.sendMessage({ tier: tier, nickname: nickname }, (response) => {});
+        chrome.runtime.sendMessage({ tier: tier, nickname: nickname, tierLevel: tierLevel, language: language }, (response) => {});
     })
 })
