442fd62ea52bb65c64b768c0ad803e78335ba373
NK590 <nathankim0917@gmail.com>
2023-04-09 16:53:50 +0900

    'update'

--- background.js
+++ background.js
@@ -1,59 +1,90 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
-    let startTier = request.tier.charAt(0);
-    let endTier = request.tier.charAt(0);
-    let tierLevel = request.tierLevel;
-    if (tierLevel != undefined && tierLevel != '') {
-        startTier += tierLevel;
-        endTier += tierLevel;
-    } else {
-        startTier += '5';
-        endTier += '1';
-    }
+    let typeOfSite = request.typeOfSite;
 
-    let language = request.language;
-    let langQuery = '';
-    if (language != undefined && language != '') {
-        langQuery = `+lang:${language}`;
-    }
-
-    let nickname = request.nickname;
-    let url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}`;
-    let problemIndex;
-
-    fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        if (data.count >= 50) {
-            let pageNum = parseInt(data.count / 50);
-            if (data.count % 50 === 0) {
-                pageNum -= 1;
-            }
-
-            let page = Math.floor(Math.random() * pageNum) + 1;
-            url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}&page=${page}`;
-            
-            fetch(url)
-            .then((response2) => response2.json())
-            .then((data2) => {
-                console.log(data2);
-                problemIndex = data2.items[Math.floor(Math.random() * data2.items.length)].problemId;
+    if (typeOfSite == 'baekjoon') {
+        let startTier = request.tier.charAt(0);
+        let endTier = request.tier.charAt(0);
+        let tierLevel = request.tierLevel;
+        if (tierLevel != undefined && tierLevel != '') {
+            startTier += tierLevel;
+            endTier += tierLevel;
+        } else {
+            startTier += '5';
+            endTier += '1';
+        }
+    
+        let language = request.language;
+        let langQuery = '';
+        if (language != undefined && language != '') {
+            langQuery = `+lang:${language}`;
+        }
+    
+        let nickname = request.nickname;
+        let url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}`;
+        let problemIndex;
+    
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.count >= 50) {
+                let pageNum = parseInt(data.count / 50);
+                if (data.count % 50 === 0) {
+                    pageNum -= 1;
+                }
+    
+                let page = Math.floor(Math.random() * pageNum) + 1;
+                url = `https://solved.ac/api/v3/search/problem?query=*${startTier}..${endTier}${langQuery}+-solved_by:${nickname}&page=${page}`;
+                
+                fetch(url)
+                .then((response2) => response2.json())
+                .then((data2) => {
+                    console.log(data2);
+                    problemIndex = data2.items[Math.floor(Math.random() * data2.items.length)].problemId;
+                    // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+                    //     chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
+                    // })
+                    chrome.tabs.create({url:`https://www.acmicpc.net/problem/${problemIndex}`});
+                })
+            } else if (0 < data.count && data.count < 50) {
+                problemIndex = data.items[Math.floor(Math.random() * data.items.length)].problemId;
                 // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
                 //     chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
                 // })
                 chrome.tabs.create({url:`https://www.acmicpc.net/problem/${problemIndex}`});
-            })
-        } else if (0 < data.count && data.count < 50) {
-            problemIndex = data.items[Math.floor(Math.random() * data.items.length)].problemId;
-            // chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-            //     chrome.tabs.sendMessage(tabs[0].id, problemIndex, (response) => {});
-            // })
-            chrome.tabs.create({url:`https://www.acmicpc.net/problem/${problemIndex}`});
-        } else {
-            chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            } else {
+                chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+                    chrome.tabs.sendMessage(tabs[0].id, "no match problems", (response) => {});
+                })
+            }
+        })
+    } else {
+        let programmersLevel = request.programmersLevel;
+
+        let url = `https://school.programmers.co.kr/api/v1/school/challenges/?levels[]=${programmersLevel}&statuses[]=unsolved`;
+        let problemIndex;
+
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.totalEntries == 0) {
                 chrome.tabs.sendMessage(tabs[0].id, "no match problems", (response) => {});
-            })
-        }
-        
-    })
+            } else {
+                let totalPages = data.totalPages;
+                let page = Math.floor(Math.random() * totalPages) + 1;
+    
+                url = `https://school.programmers.co.kr/api/v1/school/challenges/?levels[]=${programmersLevel}&statuses[]=unsolved&page=${page}`;
+    
+                fetch(url)
+                .then((response2) => response2.json())
+                .then((data2) => {
+                    let problemNum = data2.result.length;
+                    let problemRandomNum = Math.floor(Math.random() * problemNum);
+                    let problemId = data2.result[problemRandomNum].id;
+                    
+                    chrome.tabs.create({url:`https://school.programmers.co.kr/learn/courses/30/lessons/${problemId}`});
+                });
+            }
+        });
+    }
 })
--- popup/popup.js
+++ popup/popup.js
@@ -1,12 +1,32 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('typeOfSite').onchange = () => {
+        let typeOfSite = document.getElementById('typeOfSite').value;
+        if (typeOfSite == 'baekjoon') {
+            document.getElementById('baekjoonDiv').style.display = 'block';
+            document.getElementById('programmersDiv').style.display = 'none';
+        } else {
+            document.getElementById('baekjoonDiv').style.display = 'none';
+            document.getElementById('programmersDiv').style.display = 'block';
+        }
+    }
+
     document.getElementById('randBtn').addEventListener('click', () => {
         let tier = document.getElementById('tierSelect').value;
         let tierLevel = document.getElementById('tierLevelSelect').value;
         let nickname = document.getElementById('nicknameInput').value;
         let language = document.getElementById('koreanOnly').checked ? 'ko' : '';
+        let typeOfSite = document.getElementById('typeOfSite').value;
+        let programmersLevel = document.getElementById('programmersLevelSelect').value;
 
-        chrome.runtime.sendMessage({ tier: tier, nickname: nickname, tierLevel: tierLevel, language: language }, (response) => {});
+        chrome.runtime.sendMessage({ 
+            tier: tier, 
+            nickname: nickname, 
+            tierLevel: tierLevel, 
+            language: language, 
+            typeOfSite: typeOfSite,
+            programmersLevel: programmersLevel
+        }, (response) => {});
     })
 })
